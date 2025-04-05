1a83404cf33a279f6995531dda74d458ccb90c72
Duncan Morais <duncanmorais@gmail.com>
2025-04-05 15:36:32 -0600

    getting ready for deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, MatInputModule, MatFormFieldModule, MatButtonModule, MatExpansionModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'toms_venn_diagrams';
+  private _snackBar = inject(MatSnackBar);
+
+  left = signal('');
+  right = signal('');
+  intersection = signal('');
+
+  constructor(private route: ActivatedRoute) {
+    this.route.queryParams.subscribe(params => {
+      if (params['left']) {
+        this.left.set(decodeURI(params['left']))
+      }
+      if (params['right']) {
+        this.right.set(decodeURI(params['right']))
+      }
+      if (params['intersection']) {
+        this.intersection.set(decodeURI(params['intersection']))
+      }
+    });
+  }
+
+  private getShareableUrl() {
+    const url = new URL(window.location.origin);
+    url.searchParams.set('left', encodeURI(this.left()));
+    url.searchParams.set('right', encodeURI(this.right()));
+    url.searchParams.set('intersection', encodeURI(this.intersection()));
+    return url.toString();
+  }
+
+  share() {
+    const url = this.getShareableUrl();
+    navigator.clipboard.writeText(url);
+    this._snackBar.open("Copied to clipboard", 'OK', {
+      duration: 3000
+    });
+  }
 }
